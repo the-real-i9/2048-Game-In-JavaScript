674b2bf1a860a50cbc42f3febb425f072ce5468b
Mckenney <oluwarinolasam@gmail.com>
2020-08-27 20:57:46 +0100

    font reponse to tile

--- modules/initTiles.js
+++ modules/initTiles.js
@@ -1,4 +1,6 @@
-import { selectAll, setProp } from './manipFuncs.js';
+import { selectAll, setProp, select } from './manipFuncs.js';
+import DOMElems from './DOMElems.js';
+const { tileBoard } = DOMElems;
 
 const initTileCells = (tile) => {
     const allTileCells = selectAll('.tile-cell');
@@ -11,6 +13,9 @@ const initTiles = () => {
     const tiles = [2, 4];
     initTileCells(tiles[Math.trunc(Math.random() * tiles.length)]);
     initTileCells(tiles[Math.trunc(Math.random() * tiles.length)]);
+
+    const fontSize = select('.tile-cell').getBoundingClientRect().width;
+    tileBoard.style.setProperty('--font', `${fontSize}px`);
 };
 
 export default initTiles;
