e79c11a3bebc4e7b6c9e02b4d61136e88d92a708
Mckenney <oluwarinolasam@gmail.com>
2020-08-27 20:50:16 +0100

    initialize tiles

--- modules/initTiles.js
+++ modules/initTiles.js
@@ -1,7 +1,16 @@
-import { selectAll } from './manipFuncs.js';
+import { selectAll, setProp } from './manipFuncs.js';
+
+const initTileCells = (tile) => {
+    const allTileCells = selectAll('.tile-cell');
+    const empties = [...allTileCells].filter((elem) => !elem.innerHTML);
+    const randomEmptyTileCell = empties[Math.trunc(Math.random() * empties.length)];
+    setProp(randomEmptyTileCell, 'innerHTML', `<div class="tile tile-${tile}">${tile}</div>`);
+};
 
 const initTiles = () => {
-    const allTileCells = selectAll('.tile-cell');
+    const tiles = [2, 4];
+    initTileCells(tiles[Math.trunc(Math.random() * tiles.length)]);
+    initTileCells(tiles[Math.trunc(Math.random() * tiles.length)]);
 };
 
 export default initTiles;
